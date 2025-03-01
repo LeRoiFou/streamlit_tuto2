4ed3796e19d35acec6d6bac1f2805e3ec11f2365
LeRoiFou <lrcompta@gmail.com>
2025-03-01 20:07:19 +0100

    session state

--- initial_version/exercices/streamlit_advanced_features/advanced_features.py
+++ initial_version/exercices/streamlit_advanced_features/advanced_features.py
@@ -2,6 +2,10 @@
 st.form permet d'interagir des widgets avec un bouton d'exécution, qui sont 
 dans un formulaire : si on change un paramètre d'un wigdet dans un formulaire, 
 streamlit ne va pas réexécuter à nouveau l'application
+
+st.session_state permet de ne pas réinitialiser la valeur d'une valeur lorsqu'on
+réexécute l'application
+
 """
 
 import streamlit as st
@@ -28,12 +32,38 @@ def form():
         st.write(f'Your name is {name}')
         st.write(f'Are you agree? {agree}')
         st.write(f'Your color is {color}')
+        
+def session():
+    """
+    Session
+    """
+    
+    # Sous-titre
+    st.header("Streamlit session tutorial")
+    
+    # Si la variable n'est pas en session
+    if 'counter' not in st.session_state:
+        # Création d'une variable en session
+        st.session_state['counter'] = 0
+        
+    # Création d'une variable classique (qui n'est pas en session)
+    not_a_session_state = 0
+    
+    # Bouton d'exécution
+    if st.button('Click on me'):
+        # Incrémentation des deux variables ci-avant
+        st.session_state['counter'] += 1
+        not_a_session_state += 1
+        
+    st.write(st.session_state.counter, "This is a session state value")
+    st.write(not_a_session_state, "This is not a session state value")
 
 if __name__ == '__main__':
     
     # Configuration de la page web
     st.set_page_config(
         page_title='Streamlit advanced features',
+        page_icon='😎',
         layout='centered',
     )
     
@@ -42,3 +72,5 @@ if __name__ == '__main__':
 
     # Appel des fonctions ci-avant
     form()
+    session()
+    
